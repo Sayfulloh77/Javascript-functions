abe4d5ce9586658872ba0c5921f91f588c8df055
Sayfulloh77 <sayfullohabduvohidov713@gmail.com>
2025-10-19 11:02:46 +0300

    Add javascript functions

---
+++ script.js
@@ -0,0 +1,60 @@
+// getAttribute, setAttribute, Dynamic elements creat , Events, window global, objects
+"use strict"
+/*const text = document.getElementById("text")
+const login = document.getElementById("login")
+const password = document.getElementById("password")
+const span = document.getElementById("eye")*/
+
+const text = document.querySelector("#text"),
+      login = document.querySelector("#login"),
+      password = document.querySelector("#password"),
+      eye = document.querySelector("#eye"),
+      submit = document.querySelector("#submit");
+
+      console.log(text.getAttribute('class'))// attribute is calls and id
+      console.log(text.getAttribute('id'))// getAttribute shu ozgaruchini id yoki classini olib beradi
+      console.log(login.getAttribute('id'))
+
+      // login.setAttribute('type','password')
+
+      login.addEventListener('blur', ()=> {
+         if(login.value.trim().length !== 0) {
+            login.setAttribute('class','form-control p-3 m-2 success')
+         } else {
+              login.setAttribute('class','form-control p-3 m-2 error')
+         }
+      })
+      password.addEventListener('blur', ()=> {
+        if(password.value.trim().length !== 0) {
+            password.setAttribute('class','form-control p-3 m-2 success')
+        }else {
+            password.setAttribute('class','form-control p-3 m-2 error')
+        }
+      })
+
+      eye.addEventListener('click', ()=> {
+          if (password.getAttribute('type')==='password') {
+            password.setAttribute('type','text')
+          }else {
+            password.setAttribute('type','password')
+          }
+      })
+
+      submit.addEventListener('click', ()=> {
+        let loginValue = login.value.trim()
+        let passwordValue = password.value.trim()
+         
+        // Check name: only letters allowed (A-Z or a-z)
+        if (!/^[A-Za-z]+$/.test(loginValue)) {
+            login.value = "" // clear input
+            login.placeholder = "Only letters allowed!";
+            return;
+        }
+        //Check name: only numbers allowed !/^[0-9]+$/
+        if(!/^[0-9]+$/.test(passwordValue)) {
+            password.value = ""
+            password.placeholder = "Only passwords"
+            return
+        }
+        alert("Login successfully")
+      })
